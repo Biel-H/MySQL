4f4406e4c54462cb04bc2d5b35d9d51c3fe831e9
Gabriel_ <83302809+Biel-H@users.noreply.github.com>
2022-03-22 11:40:30 -0300

    Update Commands.dml.sql

--- Commands.dml.sql
+++ Commands.dml.sql
@@ -27,3 +27,47 @@ status;
 
 #To see the tables in the current database 
 show tables;
+                                                                            
+#To exclude a database                                                                                 
+drop database exampleDB;																																									 
+	
+#To improve your database you can put parameters (constrains)																																									 
+	create database exampleBD
+	default character set utf8																																								 
+	default collate utf8_general_ci;																																								 
+	
+	#To execute the command do: 																																								 
+		CTRL+ENTER																																							 
+
+#To improve your tables adding more specific fields in tables, and define a parameter (Constrains). Lastly we will add a primary key. 																																							 
+																																									 
+	create table pessoas (
+		id int NOT NULL AUTO_INCREMENT,																																							 
+		name varchar(30) NOT NULL,																																								 
+		born date,																																							 
+		sex	enum('M' , 'F'),																																						 
+		weight decimal(5,2),																																						 
+		height decimal(3,2),																																							 
+		nacionality	varchar(20) DEFAULT 'Brasil',
+		PRIMARY KEY (id)																																							 
+		) default charset = utf8;																																							 
+																																									 
+#Now we will add some datas in the tables																																									 
+	
+	INSERT INTO pessoas																																								 
+		(name, born, sex, weight, height, nacionaliy)
+ 	VALUES																																								 
+		('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil');	
+
+#you can replace the id by default too:																																									 
+
+	(DEFAULT, 'Bruno', '1999-03-22', 'M', '78.5', '1.83', 'Brasil');
+
+#Or you can put more than one value:
+	(DEFAULT 'Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
+	(DEFAULT, 'Bruno', '1999-03-22', 'M', '78.5', '1.83', 'Brasil');
+																																								 
+#If the order of the table are the same as the values you can't need to put the table, for example:
+	INSERT INTO pessoas VALUES																																								 
+		('Godofredo', '1900-01-02', 'M', '78.5', '1.83', 'Brazil');																																							 
+																																									 
